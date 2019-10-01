cc92a485c20f0084f50ca66d16b9b8b152e299d1
Yohannes Mitike <gjonmersha@gmail.com>
2019-10-01 12:25:14 +0300

    crrected

--- app/src/main/java/com/gebeya/sewoch/database/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.gebeya.sewoch.database;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {FriendList.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract FriendDAO getFriendDAO();
-}
