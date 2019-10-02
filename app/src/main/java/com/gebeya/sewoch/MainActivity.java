58de424084d93c05e6594868c91c804bf3c4cd1e
Yohannes Mitike <gjonmersha@gmail.com>
2019-10-02 08:25:16 +0300

    useless components removed

--- app/src/main/java/com/gebeya/sewoch/MainActivity.java
+++ app/src/main/java/com/gebeya/sewoch/MainActivity.java
@@ -2,56 +2,16 @@ package com.gebeya.sewoch;
 
 import android.os.Bundle;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import androidx.navigation.ui.AppBarConfiguration;
-import com.google.android.material.navigation.NavigationView;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
 public class MainActivity extends AppCompatActivity {
-    private AppBarConfiguration mAppBarConfiguration;
-    private AdView mAdView;
     private FirebaseAnalytics firebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.activity_splash_screen);
         firebaseAnalytics=FirebaseAnalytics.getInstance(this);
-        firebaseAnalytics.setMinimumSessionDuration(5000);
-
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
-
-
-
-
     }
 
 
---
+++ app/src/main/java/com/gebeya/sewoch/service/user/UserClient.java
@@ -0,0 +1,8 @@
+package com.gebeya.sewoch.service.user;
+
+
+public interface UserClient {
+    //@POST(login)
+  //  call<User> login(@Body Login login)
+
+}
